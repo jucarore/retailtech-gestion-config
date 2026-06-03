ceffd01de36df3e5f4305eda3578d48b1aed61b8
jucarore <jucarore@gmail.com>
2026-06-03 08:07:03 -0500

    fix(calc): correcion division por cero en funcion dividir

--- src/calculadora.js
+++ src/calculadora.js
@@ -2,7 +2,7 @@ function sumar(a, b) { return a + b; }
 function restar(a, b) { return a - b; }
 function multiplicar(a, b) { return a * b; }
 function 
-  dividir(a, b) { return a / 0; }
+  dividir(a, b) { return a / b; }
 function potencia(a, b) { return Math.pow(a, b); }
 module.exports = { sumar, restar, multiplicar, dividir, potencia 
   };
